f5f3495272d9f1e7117da5133cd3218401f9f138
Daniel Karlsson <led.karlsson@gmail.com>
2022-11-05 20:06:12 +0100

    Extract buttons

--- TowerDefense.py
+++ TowerDefense.py
@@ -1,6 +1,5 @@
 from tkinter import *
 from PIL import Image
-from PIL import ImageDraw
 from PIL import ImageTk
 import random
 import math
@@ -16,6 +15,8 @@ towerDictionary = {"Arrow Shooter": "ArrowShooterTower", "Bullet Shooter": "Bull
 towerCost = {"Arrow Shooter": 150, "Bullet Shooter": 150,
              "Tack Tower": 150, "Power Tower": 200}
 towerGrid = [[None for y in range(gridSize)] for x in range(gridSize)]
+
+
 pathList = []
 spawnx = 0
 spawny = 0
@@ -288,71 +289,6 @@ class NextWaveButton:
         canvas.create_rectangle(self.x, self.y, self.xTwo,
                                 self.yTwo, fill=self.color, outline=self.color)
         canvas.create_text(500, 37, text="Next Wave")
-
-
-class MyButton(object):
-    def __init__(self, x, y, xTwo, yTwo):
-        self.x = x
-        self.y = y
-        self.xTwo = xTwo
-        self.yTwo = yTwo
-
-    def checkPress(self, click, x, y):
-        if x >= self.x and y >= self.y and x <= self.xTwo and y <= self.yTwo:
-            self.pressed()
-            return True
-        return False
-
-    def pressed(self):
-        pass
-
-    def paint(self, canvas):
-        canvas.create_rectangle(self.x, self.y, self.xTwo,
-                                self.yTwo, fill="red", outline="black")
-
-
-class TargetButton(MyButton):
-    def __init__(self, x, y, xTwo, yTwo, myType):
-        super(TargetButton, self).__init__(x, y, xTwo, yTwo)
-        self.type = myType
-
-    def pressed(self):
-        global displayTower
-        displayTower.targetList = self.type
-
-
-class StickyButton(MyButton):
-    def __init__(self, x, y, xTwo, yTwo):
-        super(StickyButton, self).__init__(x, y, xTwo, yTwo)
-
-    def pressed(self):
-        global displayTower
-        if displayTower.stickyTarget == False:
-            displayTower.stickyTarget = True
-        else:
-            displayTower.stickyTarget = False
-
-
-class SellButton(MyButton):
-    def __init__(self, x, y, xTwo, yTwo):
-        super(SellButton, self).__init__(x, y, xTwo, yTwo)
-
-    def pressed(self):
-        global displayTower
-        displayTower.sold()
-        displayTower = None
-
-
-class UpgradeButton(MyButton):
-    def __init__(self, x, y, xTwo, yTwo):
-        super(UpgradeButton, self).__init__(x, y, xTwo, yTwo)
-
-    def pressed(self):
-        global money
-        global displayTower
-        if money >= displayTower.upgradeCost:
-            money -= displayTower.upgradeCost
-            displayTower.upgrade()
 
 
 class Infoboard:
