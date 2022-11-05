e61d4da2df443ce059c64ea071a914df62c4fa52
Daniel Karlsson <led.karlsson@gmail.com>
2022-11-05 21:42:27 +0100

    Reformat lists

--- TowerDefense.py
+++ TowerDefense.py
@@ -12,12 +12,23 @@ mapSize = gridSize*blockSize
 blockGrid = [[0 for y in range(gridSize)] for x in range(
     gridSize)]  # creates the array for the grid
 blockDictionary = ["NormalBlock", "PathBlock", "WaterBlock"]
-monsterDictionary = ["Monster1", "Monster2",
-                     "AlexMonster", "BenMonster", "LeoMonster", "MonsterBig"]
-towerDictionary = {"Arrow Shooter": "ArrowShooterTower", "Bullet Shooter": "BulletShooterTower",
-                   "Tack Tower": "TackTower", "Power Tower": "PowerTower"}
-towerCost = {"Arrow Shooter": 150, "Bullet Shooter": 150,
-             "Tack Tower": 150, "Power Tower": 200}
+monsterDictionary = [
+    "Monster1",
+    "Monster2",
+    "AlexMonster",
+    "BenMonster",
+    "LeoMonster",
+    "MonsterBig"]
+towerDictionary = {
+    "Arrow Shooter": "ArrowShooterTower",
+    "Bullet Shooter": "BulletShooterTower",
+    "Tack Tower": "TackTower",
+    "Power Tower": "PowerTower"}
+towerCost = {
+    "Arrow Shooter": 150,
+    "Bullet Shooter": 150,
+    "Tack Tower": 150,
+    "Power Tower": 200}
 towerGrid = [[None for y in range(gridSize)] for x in range(gridSize)]
 pathList = []
 spawnx = 0
@@ -27,8 +38,11 @@ monstersByHealth = []
 monstersByHealthReversed = []
 monstersByDistance = []
 monstersByDistanceReversed = []
-monstersListList = [monstersByHealth, monstersByHealthReversed,
-                    monstersByDistance, monstersByDistanceReversed]
+monstersListList = [
+    monstersByHealth,
+    monstersByHealthReversed,
+    monstersByDistance,
+    monstersByDistanceReversed]
 projectiles = []
 health = 100
 money = 5000000000
